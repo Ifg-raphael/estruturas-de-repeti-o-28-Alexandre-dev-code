68c30bc750db1528b8aaf6958ee989326a7390cf
Alexandre <alexandre.bessa@academico.ifg.edu.br>
2025-08-26 18:51:41 -0300

    Resolucao e documentacao

--- calcula_media.py
+++ calcula_media.py
@@ -1,1 +1,20 @@
 # Sua solução aqui
+
+# Enunciado: 
+# Faça um programa para entrar com 5 números 
+# e imprimir a média desses números.
+
+# Variavel para armazenar o somatorio
+soma = 0
+
+# Leitura de dados e armazenamento deles
+# na variavel de somatorio
+for i in range(5):
+    numero = float(input())
+    soma += numero
+
+# Calculo da media
+media = soma / 5
+
+# Saida de dados
+print(f"{media:.2f}")
